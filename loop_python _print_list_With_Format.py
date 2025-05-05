d778841a65ea0883005f8ee3b38f83cc25241d6e
ahmed_saad <ahmedsaad@gmail.com>
2025-05-05 22:45:57 +0300

    addind another proplem

---
+++ loop_python _print_list_With_Format.py
@@ -0,0 +1,12 @@
+
+names = ["ahmed" ,"ali" ,"Gamer" ,"Snake" ,"Salah" ,"Tarbon" ,"boooAhmeeeed"]
+
+
+a = 0
+
+while(a < len(names)):
+    print(f"#{str(a+1).zfill(2)} {names[a]}")
+    a+=1
+else:
+    print("loop is Done.........")
+
